c51a72d25291e535cf30c21a22a39e41b6031106
maggi1133 <tnagendrakumar99@gmail.com>
2023-11-18 09:59:29 +0530

    first script executed3

---
+++ 02 program.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "we enter the shell script pgm"
+
+Person1=nani
+Person2=maggi
+
+echo "we enter the first person name is: $Person1"
+
+echo "we enter the first second name is: $Person2"
+
