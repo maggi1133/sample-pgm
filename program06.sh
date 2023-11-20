749268339902624518cd3c908a997257b3741633
maggi1133 <tnagendrakumar99@gmail.com>
2023-11-20 13:51:53 +0530

    update shell command

---
+++ program06.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# this is smple git install package
+
+echo "we install git software package in theis session"
+
+#we check user id and install git version
+
+USERID=$(id -u)
+
+echo "user id is $USERID"
+
