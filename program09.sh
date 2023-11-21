a3f9504b60ab4d59d6a057b07dac93a1ffd49962
maggi1133 <tnagendrakumar99@gmail.com>
2023-11-21 09:47:12 +0530

    updated today commit

---
+++ program09.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# to print the special variables
+
+Date=$(date +%F)
+Script_name=$0
+Logfile=/tmp/$Script_Name-$Date.log
+
+R="\e[31m"
+G="\e[32m"
+B="\e[30m"
+
+Var1=$1
+Var2=$2
+Var3=$3
+
+echo  -e "tp print the first variable is $R sucess: $Var1 $N"
+
+echo -e "tp print the first variable is $B sucess: $Var2 $N" 
+
+echo -e "tp print the first variable is sucess: $Var3"
+
+echo -e "to print the no of variables is sucess: $@"
+
+echo -e "to print all the varaibles is sucess: $#"
+
