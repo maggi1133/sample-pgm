aefa38fd00f21e569eb7ea45b2928db7f0e29bd1
maggi1133 <tnagendrakumar99@gmail.com>
2023-11-21 11:03:16 +0530

    updated today commit

--- program09.sh
+++ program09.sh
@@ -8,19 +8,21 @@ Logfile=/tmp/$Script_Name-$Date.log
 
 R="\e[31m"
 G="\e[32m"
-B="\e[30m"
+N="\e[30m"
 
 Var1=$1
 Var2=$2
 Var3=$3
 
-echo  -e "tp print the first variable is sucess: $Var1"
+echo   "tp print the first variable is sucess: $Var1"
 
-echo -e "tp print the first variable is sucess: $Var2" 
+echo   "tp print the first variable is sucess: $Var2"
 
-echo -e "tp print the first variable is sucess: $Var3"
+echo  "tp print the first variable is sucess: $Var3" 
 
-echo -e "to print the no of variables is sucess: $@"
+echo  "tp print the first variable is sucess: $0"
 
-echo -e "to print all the varaibles is sucess: $#"
+echo "to print the no of variables is sucess: $@"
+
+echo  "to print all the varaibles is sucess: $#"
 
