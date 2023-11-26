acb1f4b2de3d07cc4850a6a936e43e1971fb04b0
maggi1133 <tnagendrakumar99@gmail.com>
2023-11-26 11:35:38 +0530

    updated shell practise

--- roboshop shell.sh/mongodb.sh
+++ roboshop shell.sh/mongodb.sh
@@ -11,6 +11,19 @@ Date=$(date +%F:%H:%M:%S)
 Script_name=$0
 Log_File=$Log_Dir/$Script_name-$Date.log
 
+
+Validate(){
+
+     if [ $1 -ne 0 ]
+     then
+          echo -e "$2 is  $R failure $N"
+          exit 1
+     else
+          echo -e "$2  $Y is sucess $N"
+     fi
+
+}
+
 # first we enter the root id is valid or not
 
 User_id=$(id -u)
@@ -22,6 +35,27 @@ User_id=$(id -u)
         echo -e " $Y installation sucessfully $N"
   fi
 
-  
+ cp /home/ec2-user/mongo.repo/   /etc/yum.repos.d/mongo.repo &>>$Log_File
+
+ Validate $? "copy the path in to that repo"
+
+yum install mongodb-org -y &>>$Log_File
+
+Validate $? "installing mongodb"
+
+systemctl enable mongod &>>$Log_File
+
+Validate $? "enable the service"
+
+systemctl start mongod &>>$Log_File
+
+Validate $? "restarting the service"
+
+sed -i 's/127.0.0.1/0.0.0.0/'  /etc/mongod.conf &>>$Log_File
+
+Validate $? "allow all the ports"
 
 
+systemctl restart mongod &>>$Log_File
+
+Validate $? "restart the mongod service"
