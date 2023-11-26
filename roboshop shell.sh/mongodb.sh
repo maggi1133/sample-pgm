d5f2ed1ba2668032d6f7cf3bbe7825de80894956
maggi1133 <tnagendrakumar99@gmail.com>
2023-11-26 11:16:55 +0530

    updated shell practise

--- roboshop shell.sh/mongodb.sh
+++ roboshop shell.sh/mongodb.sh
@@ -6,3 +6,22 @@ N="\e[0m"
 
 echo -e " $R we written to implement $N  $G roboshop shell $N"
 
+Log_Dir=/tmp
+Date=$(date +%F:%H:%M:%S)
+Script_name=$0
+Log_File=$Log_Dir/$Script_name-$Date.log
+
+# first we enter the root id is valid or not
+
+User_id=$(id -u)
+  if [ $User_id -ne 0 ];
+   then
+        echo -e " $R error:: we installed under in the root access $N"
+        exit 1
+    else
+        echo -e " $Y installation sucessfully $N"
+  fi
+
+  
+
+
